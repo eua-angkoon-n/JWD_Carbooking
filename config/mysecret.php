2a25ca98d6b56b786682b813cf1151ec542162e0
eua-angkoon-n <Eua-angkoon.n@jwdcoldchain.com>
2023-12-27 10:01:22 +0700

    add mysecret

---
+++ config/mysecret.php
@@ -0,0 +1,19 @@
+<?php
+class MySecret
+{
+    public static $conNow = 'db'; // local , db
+    public static $dbDatabase = 'db_carbooking'; 
+    public static $dbDatabaseLogin = 'db_eservice_new';
+    public static $dbUser = 'xxxxx';
+    public static $dbPass = 'xxxxx';
+    public static $dbHost = 'xxxxx'; 
+    public static $dbPort = 'xxxxx';
+
+    public static $LocalDatabase = 'xxxxxx';
+    public static $LocalDatabaseLogin = 'xxxxxxx';
+    public static $LocalUser = 'root';
+    public static $LocalPass = '';
+    public static $LocalHost = 'localhost';
+    public static $LocalPort = '';
+
+}
